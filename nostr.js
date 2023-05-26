c029d4a377f9ebdbff798ae0feeb05ea77c0f357
mroxso <mail@pascalrost.de>
2023-05-26 21:06:13 +0200

    show notes (WIP)

--- nostr.js
+++ nostr.js
@@ -18,10 +18,19 @@ async function nostrGetUserinfo() {
             const displayName = JSON.parse(data[2].content)['displayName'];
             const about = JSON.parse(data[2].content)['about'];
             const picture = JSON.parse(data[2].content)['picture'];
+            const lightningAddress = JSON.parse(data[2].content)['lud16'];
+            const website = JSON.parse(data[2].content)['website'];
             document.getElementById('header-title').innerHTML = `${displayName}`;
             // document.getElementById('username').innerHTML = `${displayName}`;
             document.getElementById('about').innerHTML = `${about}`;
+            document.getElementById('about').style = "";
             document.getElementById('picture').src = `${picture}`;
+            document.getElementById('picture').style = "";
+            document.getElementById('lud16').innerHTML = `${lightningAddress}`;
+            document.getElementById('lud16').style = "";
+            document.getElementById('website').innerHTML = `${website}`;
+            document.getElementById('website').href = `${website}`;
+            document.getElementById('website').style = "";
         } else if (data[0] === "EOSE") {
             relay.close();
         }
@@ -42,40 +51,32 @@ async function nostrGetPosts() {
         if (data[0] === "EVENT") {
             console.log(data[2].content)
 
-            // var divCol = document.createElement('div');
-            // divCol.setAttribute('class', 'col');
-            // var divCard = document.createElement('div');
-            // divCard.setAttribute('class', 'card shadow-sm');
-            // var divCardBody = document.createElement('div');
-            // divCardBody.setAttribute('class', 'card-body');
-            // var pCardText = document.createElement('p');
-            // pCardText.setAttribute('class', 'card-text');
-            // pCardText.innerHTML = content;
-            // var divBtnFlex = document.createElement('div');
-            // divBtnFlex.setAttribute('class', 'd-flex justify-content-between align-items-center');
-            // var divBtnGroup = document.createElement('div');
-            // divBtnGroup.setAttribute('class', 'btn-group');
-            // for (const tag of tags) {
-            //     divBtnGroup.innerHTML +=
-            //         `<button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='nostr://${id}';">${tag}</button>`;
-            // }
-            // var smallText = document.createElement('small');
-            // smallText.setAttribute('class', 'text-body-secondary');
-            // smallText.innerHTML = formattedTime;
+            const content = data[2].content;
+
+            var divCol = document.createElement('div');
+            divCol.setAttribute('class', 'col');
+            var divCard = document.createElement('div');
+            divCard.setAttribute('class', 'card shadow-sm');
+            var divCardBody = document.createElement('div');
+            divCardBody.setAttribute('class', 'card-body');
+            var pCardText = document.createElement('p');
+            pCardText.setAttribute('class', 'card-text');
+            pCardText.innerHTML = content;
+            var smallText = document.createElement('small');
+            smallText.setAttribute('class', 'text-body-secondary');
+            smallText.innerHTML = "formattedTime";
         
-            // var smallTextId = document.createElement('small');
-            // smallTextId.setAttribute('class', 'text-body-secondary');
-            // smallTextId.innerHTML = id;
-        
-            // divBtnFlex.appendChild(divBtnGroup);
-            // divBtnFlex.appendChild(smallText);
-            // divCardBody.appendChild(pCardText);
-            // divCardBody.appendChild(divBtnFlex);
+            var smallTextId = document.createElement('small');
+            smallTextId.setAttribute('class', 'text-body-secondary');
+            smallTextId.innerHTML = "id";
+
+            divCardBody.appendChild(pCardText);
             // divCardBody.appendChild(smallTextId);
+            // divCardBody.appendChild(smallText);
         
-            // divCard.appendChild(divCardBody);
-            // divCol.appendChild(divCard);
-            // document.getElementById('polls-row').appendChild(divCol);
+            divCard.appendChild(divCardBody);
+            divCol.appendChild(divCard);
+            document.getElementById('content').appendChild(divCol);
         } else if (data[0] === "EOSE") {
             relay.close();
         }
