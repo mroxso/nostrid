57d95e9ce3d116e2e5b589bc0affe6c75db21f4c
PascalR <24775431+mroxso@users.noreply.github.com>
2023-05-30 14:31:44 +0200

    use nostr-tools to get profile info and posts

--- nostr.js
+++ nostr.js
@@ -10,7 +10,7 @@
 // }
 
 let pubkey = window.location.href.split("/").pop();
-if(pubkey.startsWith("npub")) {
+if (pubkey.startsWith("npub")) {
     pubkey = window.NostrTools.nip19.decode(pubkey).data;
 }
 
@@ -22,89 +22,118 @@ async function nostrLogin() {
 }
 
 async function nostrGetUserinfo() {
-    const relay = new WebSocket('wss://relay.nostr.band');
+    const relay = window.NostrTools.relayInit('wss://relay.nostr.band')
+    relay.on('connect', () => {
+        console.log(`connected to ${relay.url}`)
+    })
+    relay.on('error', () => {
+        console.log(`failed to connect to ${relay.url}`)
+    })
 
-    let id = Math.floor(Math.random() * 10);
+    await relay.connect()
 
-    relay.onopen = function (event) {
-        relay.send('["REQ", "' + id + '", {"kinds": [0], "authors": ["' + pubkey + '"]}]');
-    };
-    relay.onmessage = function (event) {
-        const data = JSON.parse(event.data);
-        if (data[0] === "EVENT") {
-            console.log(data[2].content)
-            const username = JSON.parse(data[2].content)['username'];
-            const displayName = JSON.parse(data[2].content)['displayName'];
-            const name = JSON.parse(data[2].content)['name'];
-            const about = JSON.parse(data[2].content)['about'];
-            const picture = JSON.parse(data[2].content)['picture'];
-            const lightningAddress = JSON.parse(data[2].content)['lud16'];
-            const website = JSON.parse(data[2].content)['website'];
-            document.title = `${name} (@${username})`;
-            document.getElementById('header-title').innerHTML = `${name}`;
-            // document.getElementById('username').innerHTML = `${displayName}`;
-            document.getElementById('about').innerHTML = `${about}`;
-            if(about != "")
-                document.getElementById('about').style = "";
-            document.getElementById('picture').src = `${picture}`;
-            if(picture != "")
-                document.getElementById('picture').style = "";
-            document.getElementById('lud16').innerHTML = `⚡️ ${lightningAddress}`;
-            if(lightningAddress != "")
-                document.getElementById('lud16').style = "";
-            document.getElementById('website').innerHTML = `🌎 ${website}`;
-            document.getElementById('website').href = `${website}`;
-            if(website != "")
-                document.getElementById('website').style = "";
-        } else if (data[0] === "EOSE") {
-            relay.close();
+    let sub = relay.sub([
+        {
+            kinds: [0],
+            authors: [pubkey],
+            limit: 10
         }
-    }
+    ])
+    sub.on('event', data => {
+        // console.log(data.content)
+        const username = JSON.parse(data.content)['username'];
+        const displayName = JSON.parse(data.content)['displayName'];
+        const name = JSON.parse(data.content)['name'];
+        const about = JSON.parse(data.content)['about'];
+        const picture = JSON.parse(data.content)['picture'];
+        const lightningAddress = JSON.parse(data.content)['lud16'];
+        const website = JSON.parse(data.content)['website'];
+        document.title = `${name} (@${username})`;
+        document.getElementById('header-title').innerHTML = `${name}`;
+        // document.getElementById('username').innerHTML = `${displayName}`;
+        document.getElementById('about').innerHTML = `${about}`;
+        if (about != "")
+            document.getElementById('about').style = "";
+        document.getElementById('picture').src = `${picture}`;
+        if (picture != "")
+            document.getElementById('picture').style = "";
+        document.getElementById('lud16').innerHTML = `⚡️ ${lightningAddress}`;
+        if (lightningAddress != "")
+            document.getElementById('lud16').style = "";
+        document.getElementById('website').innerHTML = `🌎 ${website}`;
+        document.getElementById('website').href = `${website}`;
+        if (website != "")
+            document.getElementById('website').style = "";
+    })
+    sub.on('eose', () => {
+        sub.unsub()
+    })
 }
 
 async function nostrGetPosts() {
-    const relay = new WebSocket('wss://relay.nostr.band');
+    const relay = window.NostrTools.relayInit('wss://relay.nostr.band')
+    relay.on('connect', () => {
+        console.log(`connected to ${relay.url}`)
+    })
+    relay.on('error', () => {
+        console.log(`failed to connect to ${relay.url}`)
+    })
 
-    let id = Math.floor(Math.random() * 10);
+    await relay.connect()
 
-    relay.onopen = function (event) {
-        relay.send('["REQ", "' + id + '", {"kinds": [1], "authors": ["' + pubkey + '"], "limit": 10}]');
-    };
-    relay.onmessage = function (event) {
-        const data = JSON.parse(event.data);
-        if (data[0] === "EVENT") {
-            console.log(data[2].content)
-
-            const content = data[2].content;
-
-            var divCol = document.createElement('div');
-            divCol.setAttribute('class', 'col');
-            var divCard = document.createElement('div');
-            divCard.setAttribute('class', 'card shadow-sm');
-            var divCardBody = document.createElement('div');
-            divCardBody.setAttribute('class', 'card-body');
-            var pCardText = document.createElement('p');
-            pCardText.setAttribute('class', 'card-text');
-            pCardText.innerHTML = content;
-            var smallText = document.createElement('small');
-            smallText.setAttribute('class', 'text-body-secondary');
-            smallText.innerHTML = "formattedTime";
-        
-            var smallTextId = document.createElement('small');
-            smallTextId.setAttribute('class', 'text-body-secondary');
-            smallTextId.innerHTML = "id";
-
-            divCardBody.appendChild(pCardText);
-            // divCardBody.appendChild(smallTextId);
-            // divCardBody.appendChild(smallText);
-        
-            divCard.appendChild(divCardBody);
-            divCol.appendChild(divCard);
-            document.getElementById('content').appendChild(divCol);
-        } else if (data[0] === "EOSE") {
-            relay.close();
+    let sub = relay.sub([
+        {
+            kinds: [1],
+            authors: [pubkey],
+            limit: 10
         }
-    }
+    ])
+    sub.on('event', data => {
+        const content = data.content;
+
+        var divCol = document.createElement('div');
+        divCol.setAttribute('class', 'col');
+        var divCard = document.createElement('div');
+        divCard.setAttribute('class', 'card shadow-sm');
+        var divCardBody = document.createElement('div');
+        divCardBody.setAttribute('class', 'card-body');
+        var pCardText = document.createElement('p');
+        pCardText.setAttribute('class', 'card-text');
+        pCardText.innerHTML = content;
+        var smallText = document.createElement('small');
+        smallText.setAttribute('class', 'text-body-secondary');
+        smallText.innerHTML = "formattedTime";
+
+        var smallTextId = document.createElement('small');
+        smallTextId.setAttribute('class', 'text-body-secondary');
+        smallTextId.innerHTML = "id";
+
+        // TODO: SHOW ORIGINAL POST
+        // if(data[2].tags != null) {
+        //     var tags = document.createElement('div');
+        //     tags.setAttribute('class', 'tags');
+        //     for (let i = 0; i < data[2].tags.length; i++) {
+        //         const tag = data[2].tags[i];
+        //         var tagElement = document.createElement('span');
+
+        //         tagElement.setAttribute('class', 'badge bg-secondary');
+        //         tagElement.innerHTML = tag;
+        //         tags.appendChild(tagElement);
+        //     }
+        //     divCardBody.appendChild(tags);
+        // }
+
+        divCardBody.appendChild(pCardText);
+        // divCardBody.appendChild(smallTextId);
+        // divCardBody.appendChild(smallText);
+
+        divCard.appendChild(divCardBody);
+        divCol.appendChild(divCard);
+        document.getElementById('content').appendChild(divCol);
+    })
+    sub.on('eose', () => {
+        sub.unsub()
+    })
 }
 
 function search() {
