52c2c3decbe1443506c15978ed91f1f0386e3f6f
mroxso <mail@pascalrost.de>
2023-08-31 13:32:59 +0200

    dynamic loading wip

--- nostr.js
+++ nostr.js
@@ -254,109 +254,112 @@ async function nostrGetPosts() {
     var dataArray = []; // Create an array to store the data
 
     sub.on('event', data => {
-        dataArray.push(data); // Push each data object into the array
+        document.getElementById('notes-loading').style.display = "none";
+
+        // dataArray.push(data); // Push each data object into the array
+
+        // Your existing code to create and append elements goes here
+        // Only show posts without tags (no replies, etc.)
+        // if(data.tags.length != 0) {
+        //     return;
+        // }
+
+        // console.log(data.tags)
+
+        // Only show posts without tags (no replies, etc.)
+        for(var i = 0; i < data.tags.length; i++) {
+            if(data.tags[i][0] == "p" || data.tags[i][0] == "e") {
+                return;
+            }
+        }
+
+        const content = data.content.replace(/\r?\n/g, "<br>");
+        const formattedTime = new Date(data.created_at*1000).toLocaleString();
+        const id = data.id;
+        const encodedNoteId = window.NostrTools.nip19.noteEncode(id);
+        
+        var divCol = document.createElement('div');
+        divCol.setAttribute('class', 'col');
+        
+        var divCard = document.createElement('div');
+        divCard.setAttribute('class', 'card shadow-sm');
+        divCard.setAttribute('id', `card-${id}`);
+        
+        var divCardBody = document.createElement('div');
+        divCardBody.setAttribute('class', 'card-body');
+        
+        var pCardText = document.createElement('p');
+        pCardText.setAttribute('class', 'card-text');
+        pCardText.innerHTML = content;
+        
+        var smallTime = document.createElement('small');
+        smallTime.setAttribute('class', 'text-body-secondary');
+        smallTime.innerHTML = formattedTime;
+        
+        // Buttons
+        var pButtons = document.createElement('p');
+        var btnGroup = document.createElement('div');
+        btnGroup.setAttribute('class', 'btn-group');
+        btnGroup.setAttribute('role', 'group');
+        btnGroup.setAttribute('aria-label', 'note-button-group');
+        // Like Button
+        var btnLike = document.createElement('button');
+        var smallLikes = document.createElement('small');
+        smallLikes.setAttribute('class', 'text-body-secondary');
+        smallLikes.setAttribute('id', `likes-${id}`);
+        smallLikes.innerHTML = "0" + " 👍";
+        btnLike.setAttribute('id', `btn-like-${id}`);
+        btnLike.setAttribute('class', 'btn btn-sm btn-outline-secondary');
+        btnLike.setAttribute('onclick', `nostrLikePost(${id})`);
+        btnLike.appendChild(smallLikes);
+        
+        // Zap Button
+        var btnZap = document.createElement('button');
+        var smallZap = document.createElement('small');
+        smallZap.setAttribute('class', 'text-body-secondary');
+        smallZap.setAttribute('id', `zap-${id}`);
+        smallZap.innerHTML = "0" + " sats ⚡️";
+        btnZap.setAttribute('class', 'btn btn-sm btn-outline-secondary disabled');
+        btnZap.setAttribute('onclick', `nostrZapPost(${id})`);
+        btnZap.appendChild(smallZap);
+
+        btnGroup.appendChild(btnLike);
+        btnGroup.appendChild(btnZap);
+        pButtons.appendChild(btnGroup);
+
+        var pId = document.createElement('p');
+        var aId = document.createElement('a');
+        aId.setAttribute('class', 'text-body-secondary');
+        aId.setAttribute('href', `/n/${encodedNoteId}`);
+        aId.setAttribute('target', '_blank');
+        aId.innerHTML = encodedNoteId;
+        pId.appendChild(aId);
+        
+        divCardBody.appendChild(pCardText);
+        divCardBody.appendChild(pButtons);
+        divCardBody.appendChild(smallTime);
+        divCardBody.appendChild(pId);
+        
+        divCard.appendChild(divCardBody);
+        divCol.appendChild(divCard);
+        
+        document.getElementById('content').appendChild(divCol);
+        nostrGetLikesForPost(id);
+        nostrGetZapsForPost(id);
     });
 
     // After all data has been received (eose event), sort the dataArray and display data
     sub.on('eose', () => {
-        // dataArray.sort((a, b) => a.created_at - b.created_at);
-        dataArray.sort((a, b) => b.created_at - a.created_at);
+        // dataArray.sort((a, b) => b.created_at - a.created_at);
 
-        if(dataArray.length != 0) {
-            document.getElementById('notes-loading').style.display = "none";
-        }
+        // if(dataArray.length != 0) {
+        //     document.getElementById('notes-loading').style.display = "none";
+        // }
 
-        // Process the sorted dataArray here
-        dataArray.forEach((data) => {
-            // Your existing code to create and append elements goes here
-            // Only show posts without tags (no replies, etc.)
-            // if(data.tags.length != 0) {
-            //     return;
-            // }
-
-            // console.log(data.tags)
-
-            // Only show posts without tags (no replies, etc.)
-            for(var i = 0; i < data.tags.length; i++) {
-                if(data.tags[i][0] == "p" || data.tags[i][0] == "e") {
-                    return;
-                }
-            }
-
-            const content = data.content.replace(/\r?\n/g, "<br>");
-            const formattedTime = new Date(data.created_at*1000).toLocaleString();
-            const id = data.id;
-            const encodedNoteId = window.NostrTools.nip19.noteEncode(id);
+        // // Process the sorted dataArray here
+        // dataArray.forEach((data) => {
             
-            var divCol = document.createElement('div');
-            divCol.setAttribute('class', 'col');
-            
-            var divCard = document.createElement('div');
-            divCard.setAttribute('class', 'card shadow-sm');
-            divCard.setAttribute('id', `card-${id}`);
-            
-            var divCardBody = document.createElement('div');
-            divCardBody.setAttribute('class', 'card-body');
-            
-            var pCardText = document.createElement('p');
-            pCardText.setAttribute('class', 'card-text');
-            pCardText.innerHTML = content;
-            
-            var smallTime = document.createElement('small');
-            smallTime.setAttribute('class', 'text-body-secondary');
-            smallTime.innerHTML = formattedTime;
-            
-            // Buttons
-            var pButtons = document.createElement('p');
-            var btnGroup = document.createElement('div');
-            btnGroup.setAttribute('class', 'btn-group');
-            btnGroup.setAttribute('role', 'group');
-            btnGroup.setAttribute('aria-label', 'note-button-group');
-            // Like Button
-            var btnLike = document.createElement('button');
-            var smallLikes = document.createElement('small');
-            smallLikes.setAttribute('class', 'text-body-secondary');
-            smallLikes.setAttribute('id', `likes-${id}`);
-            smallLikes.innerHTML = "0" + " 👍";
-            btnLike.setAttribute('id', `btn-like-${id}`);
-            btnLike.setAttribute('class', 'btn btn-sm btn-outline-secondary');
-            btnLike.setAttribute('onclick', `nostrLikePost(${id})`);
-            btnLike.appendChild(smallLikes);
-            
-            // Zap Button
-            var btnZap = document.createElement('button');
-            var smallZap = document.createElement('small');
-            smallZap.setAttribute('class', 'text-body-secondary');
-            smallZap.setAttribute('id', `zap-${id}`);
-            smallZap.innerHTML = "0" + " sats ⚡️";
-            btnZap.setAttribute('class', 'btn btn-sm btn-outline-secondary disabled');
-            btnZap.setAttribute('onclick', `nostrZapPost(${id})`);
-            btnZap.appendChild(smallZap);
-
-            btnGroup.appendChild(btnLike);
-            btnGroup.appendChild(btnZap);
-            pButtons.appendChild(btnGroup);
-
-            var pId = document.createElement('p');
-            var aId = document.createElement('a');
-            aId.setAttribute('class', 'text-body-secondary');
-            aId.setAttribute('href', `/n/${encodedNoteId}`);
-            aId.setAttribute('target', '_blank');
-            aId.innerHTML = encodedNoteId;
-            pId.appendChild(aId);
-            
-            divCardBody.appendChild(pCardText);
-            divCardBody.appendChild(pButtons);
-            divCardBody.appendChild(smallTime);
-            divCardBody.appendChild(pId);
-            
-            divCard.appendChild(divCardBody);
-            divCol.appendChild(divCard);
-            
-            document.getElementById('content').appendChild(divCol);
-            nostrGetLikesForPost(id);
-            nostrGetZapsForPost(id);
-        });
+        // });
 
         sub.unsub();
     });
